f18e28547d141ec59836e0c3698f9a25cca1f457
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-02-18 23:12:55 +0100

    next

---
+++ ravenous/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SearchBar.css";
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+};
+
+export default class SearchBar extends React.Component {
+
+}
