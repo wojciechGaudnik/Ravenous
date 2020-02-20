a16a11a768eece2789e28c471636304c0b5fb146
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-02-20 23:46:17 +0100

    next

--- ravenous/src/index.js
+++ ravenous/src/index.js
@@ -6,7 +6,6 @@ import Business from "./components/Business/Business";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Business />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
